8e70ed1889458a22b40859b336b495cc6b6f9740
WF3 <wf3@wf3.fr>
2021-12-03 15:45:02 +0100

    List page

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -8,11 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/event', name: 'event_')]
 class EventController extends AbstractController
 {
+    private $eventRepository;
+
+    public function __construct(EventRepository $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
 
     #[Route('', name: 'list')]
     public function list(): Response
     {
-        return new Response("Page liste d'événements");
+        $events = $this->eventRepository->findAll();
+
+        return $this->render('event/list.html.twig', [
+            'events' => $events
+        ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
