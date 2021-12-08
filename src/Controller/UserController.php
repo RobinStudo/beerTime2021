d1871ccc0339cfe5c18810aa8296dae1873bc60d
WF3 <wf3@wf3.fr>
2021-12-08 12:30:27 +0100

    Login system

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,10 +4,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('', name: 'user_')]
@@ -45,4 +46,14 @@ class UserController extends AbstractController
         ]);
     }
 
+
+    #[Route('/login', name: 'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('user/login.html.twig', [
+            'error' => $error,
+        ]);
+    }
 }
